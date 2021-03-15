d92debcc297360693dc8926e3afaf1366150846e
Saikrally <saikrallyshaman@gmail.com>
2021-03-15 17:14:27 +0200

    New functions

--- Module_3/requests_1.py
+++ Module_3/requests_1.py
@@ -6,7 +6,6 @@ import json
 
 r = requests.get("https://merchant-staging.heropay.me/api/merchant/user/methods/100010044/17?channelId=desktop&method"
                  "=deposit&sessionId=0000-0000-0000-0000").text
-#print(r)
 p = json.loads(r)
 p1 = p['methods'][0]
 print(r)
