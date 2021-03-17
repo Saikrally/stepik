96c72828559eecccd5d5240208a6ec62b2877183
Saikrally <saikrallyshaman@gmail.com>
2021-03-17 14:33:03 +0200

    New functions

--- Module_3/test_fixture3.py
+++ Module_3/test_fixture3.py
@@ -8,7 +8,10 @@ link = "http://selenium1py.pythonanywhere.com/"
 def browser():
     print("\nstart browser for test..")
     browser = webdriver.Chrome()
-    return browser
+    yield browser
+    # этот код выполнится после завершения теста
+    print("\nquit browser..")
+    browser.quit()
 
 
 class TestMainPage1:
@@ -17,10 +20,10 @@ class TestMainPage1:
         browser.get(link)
         browser.find_element_by_css_selector("#login_link")
 
+    def test_guest_should_see_login_link1(self, browser):
+        browser.get(link)
+        browser.find_element_by_css_selector("#login_link")
+
     def test_guest_should_see_basket_link_on_the_main_page(self, browser):
         browser.get(link)
         browser.find_element_by_css_selector(".basket-mini .btn-group > a")
-
-
-if __name__ == "__main__":
-    pytest.main()
