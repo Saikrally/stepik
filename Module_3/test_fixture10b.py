1e6be62e9fbbddf2ee47560503df91dcfb56ae69
Saikrally <saikrallyshaman@gmail.com>
2021-03-18 20:42:05 +0200

    New functions

--- Module_3/test_fixture10b.py
+++ Module_3/test_fixture10b.py
@@ -28,4 +28,4 @@ class TestMainPage1:
     @pytest.mark.xfail(reason="fixing this bug right now")
     def test_guest_should_see_search_button_on_the_main_page(self, browser):
         browser.get(link)
-        browser.find_element_by_css_selector("button.favorite")
+        browser.find_element_by_css_selector("input.btn.btn-default")
