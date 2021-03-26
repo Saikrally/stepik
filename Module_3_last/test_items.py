7852f306fb871c9e6cd09d0459aeab5db452e74d
Saikrally <saikrallyshaman@gmail.com>
2021-03-26 17:43:57 +0200

    New functions

--- Module_3_last/test_items.py
+++ Module_3_last/test_items.py
@@ -1,10 +1,8 @@
 import time
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
-time.sleep(30)
 
 
 def test_add_item_to_cart(browser):
     browser.get(link)
-    browser.find_element_by_class_name("btn btn-lg btn-primary btn-add-to-basket").click()
-    assert browser.find_element_by_class_name("btn btn-lg btn-primary btn-add-to-basket")
+    browser.find_element_by_css_selector("#add_to_basket_form > button")
 
