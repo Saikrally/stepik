a8acba75133578259f44fd07f818a61ac4236b16
Saikrally <saikrallyshaman@gmail.com>
2021-03-26 15:53:56 +0200

    New functions

--- Module_3_last/confest.py
+++ Module_3_last/confest.py
@@ -15,16 +15,16 @@ def pytest_addoption(parser):
 @pytest.fixture(scope="function")
 def browser(request):
     browser_name = request.config.getoption("browser_name")
-    user_language = request.config.getoption("language")
+    language = request.config.getoption("language")
     if browser_name == "chrome":
         options = Options()
         options.add_experimental_option('prefs',
-                                        {'intl.accept_languages': user_language})
+                                        {'intl.accept_languages': language})
         print("\n\nStart chrome browser for test...")
         browser = webdriver.Chrome(options=options)
     elif browser_name == "firefox":
         fp = webdriver.FirefoxProfile()
-        fp.set_preference("intl.accept_languages", user_language)
+        fp.set_preference("intl.accept_languages", language)
         print("\n\nStart firefox browser for test...")
         browser = webdriver.Firefox(firefox_profile=fp)
     else:
---
+++ Module_3_last/test_items.py
@@ -0,0 +1,9 @@
+import time
+link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+time.sleep(30)
+
+
+def add_item_to_cart(browser):
+    browser.get(link)
+    browser.find_element_by_class_name("btn btn-lg btn-primary btn-add-to-basket")
+
