b1e66d79a0caa88e1004d17e628f6b4e968a897e
Saikrally <saikrallyshaman@gmail.com>
2021-03-26 10:05:56 +0200

    New functions

--- Module_3/conftest.py
+++ Module_3/conftest.py
@@ -2,7 +2,29 @@ import pytest
 from selenium import webdriver
 
 
+def pytest_addoption(parser):
+    parser.addoption('--browser_name', action='store', default=None,
+                     help="Choose browser: chrome or firefox")
+
+
 @pytest.fixture(scope="function")
+def browser(request):
+    browser_name = request.config.getoption("browser_name")
+    browser = None
+    if browser_name == "chrome":
+        print("\nstart chrome browser for test..")
+        browser = webdriver.Chrome()
+    elif browser_name == "firefox":
+        print("\nstart firefox browser for test..")
+        browser = webdriver.Firefox()
+    else:
+        raise pytest.UsageError("--browser_name should be chrome or firefox")
+    yield browser
+    print("\nquit browser..")
+    browser.quit()
+
+
+"""@pytest.fixture(scope="function")
 def browser():
     print("\nstart browser for test..")
     options = webdriver.ChromeOptions()
@@ -10,4 +32,4 @@ def browser():
     browser = webdriver.Chrome(options=options)
     yield browser
     print("\nquit browser..")
-    browser.quit()
+    browser.quit()"""
