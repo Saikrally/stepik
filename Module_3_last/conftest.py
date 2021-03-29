67efab1d60861a9443bbbd19c0662cc5e70a1e17
Saikrally <saikrallyshaman@gmail.com>
2021-03-29 10:17:44 +0300

    New functions

--- Module_3_last/conftest.py
+++ Module_3_last/conftest.py
@@ -1,32 +1,5 @@
 import pytest
 from selenium import webdriver
-
-
-def pytest_addoption(parser):
-    parser.addoption('--browser_name', action='store', default="chrome",
-                     help="Choose browser: chrome or firefox")
-
-
-@pytest.fixture(scope="function")
-def browser(request):
-    browser_name = request.config.getoption("browser_name")
-    browser = None
-    if browser_name == "chrome":
-        print("\nstart chrome browser for test..")
-        options = webdriver.ChromeOptions()
-        options.add_experimental_option('excludeSwitches', ['enable-logging'], )
-        browser = webdriver.Chrome(options=options)
-    elif browser_name == "firefox":
-        print("\nstart firefox browser for test..")
-        browser = webdriver.Firefox()
-    else:
-        raise pytest.UsageError("--browser_name should be chrome or firefox")
-    yield browser
-    print("\nquit browser..")
-    browser.quit()
-
-"""import pytest
-from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
 
@@ -44,10 +17,12 @@ def browser(request):
     browser_name = request.config.getoption("browser_name")
     language = request.config.getoption("language")
     if browser_name == "chrome":
+        print("\nstart chrome browser for test..")
         options = Options()
         options.add_experimental_option('prefs',
                                         {'intl.accept_languages': language})
-        print("\n\nStart chrome browser for test...")
+        options = webdriver.ChromeOptions()
+        options.add_experimental_option('excludeSwitches', ['enable-logging'], )
         browser = webdriver.Chrome(options=options)
     elif browser_name == "firefox":
         fp = webdriver.FirefoxProfile()
@@ -60,6 +35,3 @@ def browser(request):
     print("\nQuit browser...")
     browser.quit()
 
-
-
-"""
