a998ab5f3e23c50fb5e4df516e94ecc984ce30b7
Saikrally <saikrallyshaman@gmail.com>
2021-03-31 11:05:34 +0300

    change login link

--- Module_4_stepik/test_main_page.py
+++ Module_4_stepik/test_main_page.py
@@ -2,8 +2,8 @@ link = "http://selenium1py.pythonanywhere.com/"
 
 
 def go_to_login_page(browser):
-    link = browser.find_element_by_css_selector("#login_link")
-    link.click()
+    login_link = browser.find_element_by_css_selector("#login_link")
+    login_link.click()
 
 
 def test_guest_can_go_to_login_page(browser):
